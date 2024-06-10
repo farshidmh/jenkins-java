d151de8b47a5c79eb0e0dfb86c1188cbf24cabaf
Farshid Mehrtash <mehrtash.fm@live.com>
2024-06-10 12:05:41 +0200

    init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+     public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.Assert.assertEquals;
+import org.example.App;
+import org.junit.Test;
+
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(app.add(1, 2), 3);
+    }
+
+    @Test
+    public void testSub() {
+        App app = new App();
+        assertEquals(app.sub(2, 1), 1);
+    }
+
+
+}
